36e26847f85e3fd07cd8d996262c9158286d3071
kefeimo <kefeimo@gmail.com>
2021-05-13 23:13:38 -0700

    Initial commit

---
+++ src/main/java/javaHello/javaHelloWorld.java
@@ -0,0 +1,7 @@
+package javaHello;
+
+public class javaHelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
---
+++ src/main/scala/scalaHello.scala
@@ -0,0 +1,6 @@
+object scalaHello {
+  def main(args: Array[String]): Unit = {
+    println("Hello scala")
+  }
+
+}
